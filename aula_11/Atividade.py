5b7c1de96bb78b5577f33e3f0f76630633dbe61d
gabriel <g27566680@gmail.com>
2026-03-31 11:26:39 -0300

    AT3

--- aula_11/Atividade.py
+++ aula_11/Atividade.py
@@ -1,6 +1,15 @@
-idade = int(input("Informe sua idade: "))
-salário = float(input("Informe sua renda mensal: "))
-tempo_de_trabalho = float(input("Informe seu tempo de serviço em anos: "))
+idade = int(input("Informe sua idade:"))
+salário = float(input("Informe sua renda mensal:"))
+tempo_de_trabalho = float(input("Informe seu tempo de serviço em anos:"))
+
+## Explicação do uso de Int e Float no código:
+# O comando input() sempre lê o que o usuário digita como um texto (string).
+# Para fazermos contas matemáticas ou comparações, precisamos converter esse texto:
+# int()   -> Converte para NÚMEROS INTEIROS (sem casas decimais). 
+#            Exemplo: Idade, quantidade de filhos, meses (18, 25, 3).
+
+# float() -> Converte para NÚMEROS REAIS/DECIMAIS (com casas decimais). 
+#            Exemplo: Dinheiro, altura, tempo fracionado (2500.50, 1.75, 2.5).
 
 
 
@@ -10,7 +19,7 @@ if idade < 18:
  print("empréstimo negado por conta da idade")
 elif salário >= 5000:
  print("empréstimo aprovado com sucesso por conta da renda")
-elif idade >= 18 and salário >= 2000 and tempo_de_trabalho>=2:
+elif idade >= 18 and salário >= 2000 and tempo_de_trabalho >=2:
  print("empréstimo aprovado")
 
 else:
